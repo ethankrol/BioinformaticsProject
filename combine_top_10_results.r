156c7e4ecbc29ea0ea802cbcced47ee02ed72e67
Nicco <nturillo@ufl.edu>
2024-09-25 22:51:20 -0400

    renamed clusterProfiler

---
+++ combine_top_10_results.r
@@ -0,0 +1,6 @@
+source("config.r")
+
+gProfiler2_Gene_Ontology_top10_file <- file.path(results_dir, "gProfiler2_Gene_Ontology_top_20_gsea_go_enrichment_results.tsv")
+clusterProfiler_top10_file <- file.path(results_dir, "SRP062929_cluserProfiler_HallmarkONTO_top_10_gsea_results.tsv")
+topGO_top10_file <- file.path(results_dir, "topGO_MF_top_10_results.tsv")
+wilcoxon_top10_file <- file.path(results_dir, "wilcoxon_rank_sum_test_top_10_results.tsv")
