3370da5fd978c4767da49f170368998bea8eda47
Ethan Krol <148501957+ethankrol@users.noreply.github.com>
2024-10-23 20:16:57 -0400

    Update assignment_2_heatmap_generation.r

--- assignment_2_heatmap_generation.r
+++ assignment_2_heatmap_generation.r
@@ -1,27 +1,33 @@
-library(mclust)
 library(pheatmap)
-
-filtered_expression_df <- readr::read_tsv(/results/filtered_data.tsv) %>%
-  tibble::column_to_rownames("Symbol")
-gene_variances <- apply(filtered_expression_df, 1, var)
-top_5000_genes <- order(gene_variances, decreasing = TRUE)[1:5000]
-top_5000_gene_data <- filtered_expression_df[top_5000_genes, ]
-
-#Generate MClust clustering for 5000 genes if needed
-mclust_top_5000 = t(top_5000_gene_data)
-data_scaled <- scale(mclust_top_5000)
-gmm_model_5000 = Mclust(data_scaled)
-clusters_5000 <- gmm_model_5000$classification
 
 scaled_top_5000_genes <- t(scale(t(top_5000_gene_data)))
 
 filtered_metadata <- readr::read_tsv("/results/filtered_metadata.tsv")
 
-#These are the row annotations for the heatmap. The variables are just a column with the clustering groups for each of the 55 samples.
+spectral_data <- readr::read_tsv("/results_assignment3/2_spectral_clusters_from_5000_genes.tsv")
+
+Kmeans_data <- readr::read_tsv("/results_assignment3/K_means/K_means_6_clusters_from_5000_genes.tsv")
+
+pam_data <- readr::read_tsv("/results_assignment3/5_pam_clusters_from_5000_genes.tsv")
+
+affinity_data <- readr::read_tsv("results_assignment3/9_affinity_propagation_clusters_from_5000_genes.tsv")
 
 heatmap_annotations <- data.frame(
-  Cluster_5000_genes = as.factor(clusters_5000),
+  GMM_Cluster_5000_genes = as.factor(clusters_5000),
+  K_means_cluster_5000_genes = as.factor(Kmeans_data$Cluster),
+  spectral_cluster_5000_genes = as.factor(spectral_data$Cluster),
+  pam_cluster_5000_genes = as.factor(pam_data$Cluster),
+  affinity_cluster_5000_genes = as.factor(affinity_data$Cluster),
   Sample_Group= as.factor(filtered_metadata$time_status)
+)
+
+ann_colors <- list(
+  GMM_Cluster_5000_genes = c("1" = "blue", "2" = "cyan", "3" = "magenta", "4" = "orange", "5" = "green", "6" = "purple", "7" = "pink"),
+  K_means_cluster_5000_genes = c("0" = "red", "1" = "yellow", "2" = "limegreen", "3" = "skyblue", "4" = "salmon", "5" = "gold"),
+  spectral_cluster_5000_genes = c("0" = "darkblue", "1" = "lightgreen"),
+  pam_cluster_5000_genes = c("1" = "darkred", "2" = "darkgreen", "3" = "darkorange", "4" = "darkviolet", "5" = "gray"),
+  affinity_cluster_5000_genes = c("0" = "lightpink", "1" = "lightcyan", "2" = "orange", "3" = "lightgreen", "4" = "lightblue", "5" = "lightcoral", "6" = "lightgoldenrod", "7" = "lightsalmon", "8" = "lightseagreen"),
+  Sample_Group = c("One_Day_After_Defeat" = "red", "One_Month_After_Defeat" = "blue")  
 )
 
 
@@ -34,5 +40,6 @@ heatmap_plot <- pheatmap(
   show_rownames = FALSE,
   show_colnames = TRUE,
   annotation_col = heatmap_annotations,
+  annotation_colors = ann_colors,
   main = "Heatmap of 5,000 Most Variable Genes with Clustering Annotations"
 )
